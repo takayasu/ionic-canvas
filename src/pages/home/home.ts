75eb5e9994c0958d7e1da90a10332ae7616b5e2c
高安厚思 <a.takayasu@gmail.com>
2018-03-23 20:44:55 +0900

    Random Show2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -28,11 +28,16 @@ export class HomePage {
   // @embed("imgs/2016ssk_akb068.jpg")
   private image: any;
 
+  private imagePoint: any;
 
   constructor(public navCtrl: NavController) {
 
     this.image = new Image();
     this.image.src = 'http://art17.photozou.jp/pub/996/3139996/photo/231969749_624.v1520733748.jpg';
+
+    this.imagePoint = new Image();
+    this.imagePoint.src = 'https://assets.chatwork.com/images/emoticon2x/emo_lucky.gif';
+
   }
 
   ionViewDidLoad() {
@@ -77,12 +82,66 @@ export class HomePage {
     this.clearCanvas();
 
     this._CONTEXT.beginPath();
-    this._CONTEXT.drawImage(this.image, 0, 0);
+    this.drawBackGround();
+    this._CONTEXT.drawImage(this.imagePoint, 30, 30);
     this._CONTEXT.arc(this._CANVAS.width / 2, 200, 80, 0, 2 * Math.PI);
     this._CONTEXT.lineWidth = 5;
     this._CONTEXT.strokeStyle = '#ffff00';
     this._CONTEXT.stroke();
 
   }
+
+  drawBackGround(){
+    this._CONTEXT.drawImage(this.image, 0, 0);
+  }
+
+
+  drawSequence(points: any[]){
+
+    points.forEach((value,number) => {
+      this._CONTEXT.drawImage(this.imagePoint, value.x, value.y);
+    })
+
+    this._CONTEXT.restore();
+
+  }
+
+  randomIcon(){
+    this.clearCanvas();
+    this.drawBackGround();
+    this.drawSequence(this.randomPoints());
+  }
+
+  randomPoints() :any[] {
+
+    let loop = Math.floor(Math.random() * 6) +50;
+
+    let points :any[] = new Array(loop+1);
+
+
+
+    for(var i = 0; i < loop ; i++){
+      points[i] = this.randomPoint();
+    }
+
+    return points;
+  }
+
+  randomPoint() :any{
+
+  let min = 20;
+  let max = this._CANVAS.width - 20 ;
+
+  let value = {
+    x: Math.floor(Math.random() * (max - min +1)) + min,
+    y: Math.floor(Math.random() * (max - min +1)) + min
+  };
+
+  return value;
+  }
+
+
+
+
 
 }
